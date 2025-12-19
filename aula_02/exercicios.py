b63ab16396752bfa913123f3cb05a6b8ed948c5c
IgorNatann <Igornatan4@gmail.com>
2025-12-19 15:59:35 -0300

    adiciona exercícios de operações básicas com inteiros

--- aula_02/exercicios.py
+++ aula_02/exercicios.py
@@ -1,10 +1,39 @@
 # #### Inteiros (`int`)
 
 # 1. Escreva um programa que soma dois números inteiros inseridos pelo usuário.
+number_1 = int(input("Digite o primeiro numero inteiro: "))
+number_2 = int(input("Digite o segundo numero inteiro:"))
+
+result = number_1 + number_2
+
+print(f"A soma de {number_1} e {number_2} é: {result}")
 # 2. Crie um programa que receba um número do usuário e calcule o resto da divisão desse número por 5.
+number = int(input("Digite um numero inteiro: "))
+
+result = number % 5
+
+print(f"O resto da divisão de {number} por 5 é: {result}")
 # 3. Desenvolva um programa que multiplique dois números fornecidos pelo usuário e mostre o resultado.
+number_1 = int(input("Digite o primeiro numero inteiro: "))
+number_2 = int(input("Digite o segundo numero inteiro: "))
+
+result = number_1 * number_2
+
+print(f"O produto de {number_1} e {number_2} e: {result}")
+
 # 4. Faça um programa que peça dois números inteiros e imprima a divisão inteira do primeiro pelo segundo.
+number_1 = int(input("Digite o primeiro numero inteiro: "))
+number_2 = int(input("Digite o segundo numero inteiro: "))
+
+result = number_1 // number_2
+
+print(f"A divisao inteira de {number_1} por {number_2} e: {result}")
 # 5. Escreva um programa que calcule o quadrado de um número fornecido pelo usuário.
+number = int(input("Digite um numero inteiro: "))
+
+result = number ** 2
+
+print(f"O quadrado de {number} e: {result}")
 
 # #### Números de Ponto Flutuante (`float`)
 
