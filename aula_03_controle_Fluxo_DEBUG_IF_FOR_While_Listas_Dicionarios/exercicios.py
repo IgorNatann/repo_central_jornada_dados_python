538286ebe72e010b68e365a10946db516588a6e0
IgorNatann <Igornatan4@gmail.com>
2026-01-20 17:43:46 -0300

    feat: adiciona verificação de validade para quantidade e preço

--- aula_03_controle_Fluxo_DEBUG_IF_FOR_While_Listas_Dicionarios/exercicios.py
+++ aula_03_controle_Fluxo_DEBUG_IF_FOR_While_Listas_Dicionarios/exercicios.py
@@ -3,6 +3,15 @@
 # que todos os registros tenham valores positivos para `quantidade` e `preço`. 
 # Escreva um programa que verifique esses campos e imprima "Dados válidos" se ambos 
 # forem positivos ou "Dados inválidos" caso contrário.
+quantidade = 10
+preco = 25.5   
+
+try:
+    if quantidade > 0 and preco > 0:
+        print("Dados válidos")
+    else:
+        print("Dados inválidos")
+
 
 ### Exercício 2: Classificação de Dados de Sensor
 # Imagine que você está trabalhando com dados de sensores IoT. 
