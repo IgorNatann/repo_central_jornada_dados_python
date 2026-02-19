29fa3595701867f7e699601631c2002635ae3dbc
IgorNatann <Igornatan4@gmail.com>
2026-02-19 16:13:01 -0300

    feat(aula-04): adicionar resolução exercício 2 e 3

--- aula_04_Tipos_complexos_TypeHint(Dict-DFs-Tables)/exercicios_list_dict.py
+++ aula_04_Tipos_complexos_TypeHint(Dict-DFs-Tables)/exercicios_list_dict.py
@@ -6,6 +6,25 @@ for numero in numeros:
     print(numero ** 2)
 
 # 2- Dada a lista ["Python", "Java", "C++", "JavaScript"], remova o item "C++" e adicione "Ruby".
+
+linguagens: list[str] = ["Python", "Java", "C++", "JavaScript"]
+linguagens.remove("C++")
+linguagens.append("Ruby")
+print(linguagens)
+
 # 3- Crie um dicionário para armazenar informações de um livro, incluindo título, autor e ano de publicação. Imprima cada informação.
+
+livros = {
+    "titulo": "O Senhor dos Anéis",
+    "autor": "J.R.R. Tolkien",
+    "ano_publicacao": 1954
+}
+
+print(f"Título: {livros['titulo']}")
+print(f"Autor: {livros['autor']}")
+print(f"Ano de Publicação: {livros['ano_publicacao']}")
+print(f"{livros}")
+
+
 # 4- Escreva um programa que conta o número de ocorrências de cada caractere em uma string usando um dicionário.
 # 5- Dada a lista ["maçã", "banana", "cereja"] e o dicionário {"maçã": 0.45, "banana": 0.30, "cereja": 0.65}, calcule o preço total da lista de compras.
